6efdc6659361028a2f546b83d19a3249b96e9033
Jofrey Luc <jyeil9@laposte.net>
2018-02-11 12:21:23 +0100

    Renommage

--- ball.h
+++ ball.h
@@ -10,13 +10,14 @@ public:
 
     void collideWith(Wall w);
     void strike(double dir, double speed);
-    void updateSpeed();
+    void updateVelocity();
 
 private:
+    const double ACCELERATION = 0.8;
     double m_posX;
     double m_posY;
     double m_direction;
-    double m_speed;
+    double m_velocity;
 };
 
 #endif // BALL_H
