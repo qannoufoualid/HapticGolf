e72c28ba5f53a098b7b4a70ca18f097bab1d9b9b
Jofrey Luc <jyeil9@laposte.net>
2018-02-18 22:32:36 +0100

    objets modele dans mainwondow

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,13 +1,20 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include "clickablelabel.h"
-#include <QLabel>
-#include <QDebug>
+
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
+    // Setup modèle
+    std::vector<Wall> walls = {
+        Wall(Point(10,10), Point(50,10), false)
+    };
+    m_course = new Course(Point(0,0), Point(100,100), walls);
+    m_ball = new Ball(m_course->getTee());
+    // A LIBERER
+
     ui->setupUi(this);
 
     ClickableLabel* ballLabel = new ClickableLabel();
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,13 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QLabel>
+#include <QDebug>
+#include <vector>
+
+#include "ball.h"
+#include "course.h"
+#include "wall.h"
 
 namespace Ui {
 class MainWindow;
@@ -21,6 +28,8 @@ public slots:
 
 private:
     Ui::MainWindow *ui;
+    Course* m_course;
+    Ball* m_ball;
 };
 
 #endif // MAINWINDOW_H
